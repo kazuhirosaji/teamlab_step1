d2ee0c5a498903da722e86786becd026803a968c
kazuhirosaji <kazuhirosaji@gmail.com>
2016-02-06 03:11:25 +0900

    ToDoリスト作成 残項目: HTMLエスケープ処理

---
+++ todo.js
@@ -0,0 +1,70 @@
+$(loaded);
+
+function loaded() {
+  showText();
+  // ボタンをクリックしたときに実行するイベントを設定する
+  $("#formButton").click(
+    // コールバックとしてメソッドを引数にわたす
+    function() {
+      saveText();
+      showText();
+    });
+}
+
+// 入力された内容をローカルストレージに保存する
+function saveText() {
+  // 時刻をキーにして入力されたテキストを保存する
+  var text = $("#formText");
+  if (checkText(text.val())) {
+    var time = new Date();
+    localStorage.setItem(time, text.val());
+    // テキストボックスを空にする
+    text.val("");
+  }
+}
+
+// ローカルストレージに保存した値を再描画する
+function showText() {
+  // すでにある要素を削除する
+  var list = $("#list")
+  list.children().remove();
+  // ローカルストレージに保存された値すべてを要素に追加する
+  var key, value, html = [];
+  for(var i=localStorage.length - 1; i >= 0; i--) {
+    key = localStorage.key(i);
+    value = localStorage.getItem(key);
+    html.push("<li>" + value + "</li>");
+  }
+
+  list.append(html.join(''));
+}
+
+
+// 文字をエスケープする
+function escapeText(text) {
+  return $("#newitem").html("<li>" + text + "</li>");
+}
+
+// 入力チェックを行う
+function checkText(text) {
+  // 文字数が0または20以上は不可
+  if (0 === text.length || 20 < text.length) {
+    alert("文字数は1〜20字にしてください");
+    return false;
+  }
+
+  // すでに入力された値があれば不可
+  var length = localStorage.length;
+  for (var i = 0; i < length; i++) {
+    var key = localStorage.key(i);
+    var value = localStorage.getItem(key);
+    // 内容が一致するものがあるか比較
+    if (text === value) {
+      alert("同じ内容は避けてください");
+      return false;
+    }
+  }
+
+  // すべてのチェックを通過できれば可
+  return true;
+}
